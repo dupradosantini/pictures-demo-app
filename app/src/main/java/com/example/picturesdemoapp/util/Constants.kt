0903f4dba018455f1e716a2029af490ea6a69016
Luis Santini <dupradosantini@opus-software.com.br>
2023-03-27 14:59:13 -0300

    Creating table for remote keys

--- app/src/main/java/com/example/picturesdemoapp/util/Constants.kt
+++ app/src/main/java/com/example/picturesdemoapp/util/Constants.kt
@@ -4,6 +4,8 @@ object Constants {
 
     const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
 
+    const val UNSPLASH_REMOTE_KEYS_TABLE = "unsplash_remote_keys_table"
+
     const val BASE_URL = "https://api.unsplash.com"
 
 }
