0f762745eddfcc640c860cdcc1c456321b7f6368
Luis Santini <dupradosantini@opus-software.com.br>
2023-03-27 14:55:24 -0300

    Dagger Hilt network module.

---
+++ app/src/main/java/com/example/picturesdemoapp/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.example.picturesdemoapp.di
+
+import com.example.picturesdemoapp.data.remote.UnsplashApi
+import com.example.picturesdemoapp.util.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15,TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(MediaType.get("application/json")))
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideUnsplashApi(retrofit: Retrofit): UnsplashApi{
+        return retrofit.create(UnsplashApi::class.java)
+    }
+
+}
--- app/src/main/java/com/example/picturesdemoapp/util/Constants.kt
+++ app/src/main/java/com/example/picturesdemoapp/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
 
+    const val BASE_URL = "https://api.unsplash.com"
+
 }
