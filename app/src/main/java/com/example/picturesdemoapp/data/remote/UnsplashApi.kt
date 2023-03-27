35f8c591a581d3a60366f5c99ce58a30847f57c8
Luis Santini <dupradosantini@opus-software.com.br>
2023-03-27 14:08:54 -0300

    Adding the first API methods

---
+++ app/src/main/java/com/example/picturesdemoapp/data/remote/UnsplashApi.kt
@@ -0,0 +1,27 @@
+package com.example.picturesdemoapp.data.remote
+
+import com.example.picturesdemoapp.BuildConfig
+import com.example.picturesdemoapp.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImages(
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+
+
+}
